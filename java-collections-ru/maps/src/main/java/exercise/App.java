dcfee242965118c4512b21c3df7aa86a394361bb
@hexlet/cli <support@hexlet.io>
2023-06-15 02:00:07 +0400

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,41 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+// BEGIN
+class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+
+        if (sentence.length() == 0) {
+            return result;
+        }
+
+        List<String> words = List.of(sentence.split(" "));
+
+        for (String word : words) {
+            Integer count = result.getOrDefault(word, 0) + 1;
+            result.put(word, count);
+        }
+
+        return result;
+    }
+
+    public static String toString(Map<String, Integer> wordsCount) {
+        if (wordsCount.size() == 0) {
+            return "{}";
+        }
+
+        StringJoiner result = new StringJoiner("\n", "{\n", "\n}");
+
+        for (Map.Entry<String, Integer> word : wordsCount.entrySet()) {
+            result.add(String.format("  %s: %s", word.getKey(), word.getValue()));
+        }
+
+        return result.toString();
+    }
+}
+//END
