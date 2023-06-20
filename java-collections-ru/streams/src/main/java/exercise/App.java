a9eb9f400b5ce2d62efde434d4e96ffd803a11a7
@hexlet/cli <support@hexlet.io>
2023-06-20 23:23:32 +0400

    submit java-collections-ru/streams

--- java-collections-ru/streams/src/main/java/exercise/App.java
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -5,9 +5,12 @@ import java.util.List;
 // BEGIN
 class App {
     public static Long getCountOfFreeEmails(List<String> emailsList) {
-        return emailsList.stream()
-                .filter(App::isFreeDomain)
-                .count();
+        if (emailsList != null) {
+            return emailsList.stream()
+                    .filter(App::isFreeDomain)
+                    .count();
+        } else
+            return 0L;
     }
 
     public static Boolean isFreeDomain(String email) {
