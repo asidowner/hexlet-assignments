5305953e5f1aa54a881c260af0b62857843ffdb9
@hexlet/cli <support@hexlet.io>
2024-01-05 20:51:57 +0400

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -3,11 +3,18 @@ package exercise.controller;
 import exercise.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 import exercise.model.Person;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/people")
